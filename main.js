ad88d202609b90557c53e5098797bb998a379d63
Anna Sokolowska <milanka.anka@gmail.com>
2022-07-23 15:30:41 +0200

    get const

---
+++ main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const modalTitle = document.querySelector('.modal__title');
+console.log(modalTitle);
+const modalForm = document.querySelector('.modal__form');
+console.log(modalForm);
+const modalCheckbox = document.querySelector('.modal__checkbox');
+console.log(modalCheckbox);
+const modalInputDiscount = document.querySelector('.modal__input_discount');
+console.log(modalInputDiscount);
